1f73cd245e3380799d901ef961fb9552a462c149
TristanRS <titi120605@gmail.com>
2024-12-07 17:07:00 +0100

    entite_2

--- src/Entite.java
+++ src/Entite.java
@@ -1,82 +1,112 @@
-package src;
+package src; 
 
 import java.awt.Color;
 
-public abstract class Entite {
+public abstract class Entite { // Classe abstraite Entite (représente les entités du jeu : tours, ennemis)
     
-    enum Element {
-        
+    enum Element { // Enumération des éléments
+
         Neutre(Color.BLACK),
-        Feu(new Color(184, 22, 1)),
-        Eau(new Color(6, 0, 160)),
-        Terre(new Color(0, 167, 15)),
-        Air(new Color(242, 211, 0));
+        Feu(new Color(184, 22, 1)),   //Element feu
+        Eau(new Color(6, 0, 160)),    //Element eau
+        Terre(new Color(0, 167, 15)), //Element terre
+        Air(new Color(242, 211, 0));  //Element air
 
-        private final Color color;
+        private final Color color; // Couleur associée à l'élément
 
-        Element(Color color) {
+        Element(Color color) {     // Constructeur
             this.color = color;
         }
 
-        public Color getColor() {
+        public Color getColor() {  // Getter
             return color;
         }
 
     };
 
-    private int PV;
-    private int ATK;
-    private double ATKSpeed;
-    private int Range;
-    private Element element;
+    enum ModeAttaque {  // Enumération des modes d'attaque
+        SINGLE_TARGET,         // Attaque une seule cible
+        MULTIPLE_TARGET,       // Attaque plusieurs cibles
+        RANDOM,                // Attaque une cible aléatoire
+        NEAREST,               // Attaque la cible la plus proche
+        STRONGEST_ATK,         // Attaque la cible avec l'attaque la plus élevée
+        STRONGEST_PERCENT_PV;  // Attaque la cible avec le pourcentage de PV le plus élevé
+    }
 
-    public Entite(int PV, int ATK, double ATKSpeed, int Range, Element element) {
+    private int PV;          // Points de vie
+    private int ATK;         // Points d'attaque
+    private double ATKSpeed; // Vitesse d'attaque
+    private int Range;       // Portée d'attaque
+    private Element element; // Element de l'entité
+    private Tile position;   // Position de l'entité
+    private ModeAttaque modeAttaque; // Mode d'attaque
+
+    public Entite(int PV, int ATK, double ATKSpeed, int Range, Element element, Tile position, ModeAttaque modeAttaque) { // Constructeur
         this.PV = PV;
         this.ATK = ATK;
         this.ATKSpeed = ATKSpeed;
         this.Range = Range;
         this.element = element;
+        this.position = position;
+        this.modeAttaque = modeAttaque;
     }
 
-    public int getPV() {
+    public int getPV() { // Getter
         return PV;
     }
 
-    public void setPV(int PV) {
+    public void setPV(int PV) { // Setter
         this.PV = PV;
     }
 
-    public int getATK() {
+    public int getATK() { // Getter
         return ATK;
     }
 
-    public void setATK(int ATK) {
+    public void setATK(int ATK) { // Setter
         this.ATK = ATK;
     }
 
-    public double getATKSpeed() {
+    public double getATKSpeed() { // Getter
         return ATKSpeed;
     }
 
-    public void setATKSpeed(double ATKSpeed) {
+    public void setATKSpeed(double ATKSpeed) { // Setter
         this.ATKSpeed = ATKSpeed;
     }
 
-    public int getRange() {
+    public int getRange() { // Getter
         return Range;
     }
 
-    public void setRange(int Range) {
+    public void setRange(int Range) { // Setter
         this.Range = Range;
     }
 
-    public Element getElement() {
+    public Element getElement() { // Getter
         return element;
     }
 
-    public void setElement(Element element) {
+    public void setElement(Element element) { // Setter
         this.element = element;
     }
 
+    public Tile getPosition() { // Getter
+        return position;
+    }
+
+    public void setPosition(Tile position) { // Setter
+        this.position = position;
+    }
+
+    public ModeAttaque getModeAttaque() { // Getter
+        return modeAttaque;
+    }
+
+    public void setModeAttaque(ModeAttaque modeAttaque) { // Setter
+        this.modeAttaque = modeAttaque;
+    }
+
+    
     
 }
