3412ea2958c89c3555eb88e8aaec2b2e2967f08b
Kiyan Le Gall <kiyan.legall@etudiant.univ-rennes.fr>
2025-01-03 22:11:40 +0100

    buffer ok

--- src/Monsters/Buffer.java
+++ src/Monsters/Buffer.java
@@ -1,5 +1,9 @@
 package src.Monsters;
 
+import java.util.Collections;
+import java.util.List;
+import java.util.stream.Collectors;
+
 import src.*;
 
 public class Buffer extends Ennemy {
@@ -12,6 +16,32 @@ public class Buffer extends Ennemy {
      * @param map       Référence à la carte associée.
      */
     public Buffer(double spawnTime, double x, double y, Map map) {
-        super(10, 2, 2.0, 3, Element.Air, 0.5, spawnTime, x, y, map);
+        super(10, 2, 1.0, 3, Element.Air, 0.5, spawnTime, x, y, map, 5, ModeAttaque.RANDOM);
+    }
+
+    @Override
+    public List<Warrior> selectTargets(ModeAttaque mode, List<Warrior> ennemis) {
+        // Sélectionne une tour aléatoire à attaquer
+        return ennemis.isEmpty() ? Collections.emptyList() : List.of(ennemis.get((int) (Math.random() * ennemis.size())));
+    }
+
+    @Override
+    public void attaquer(Warrior cible) {
+        // Augmente passivement la vitesse d'attaque et de déplacement des ennemis proches
+        double cellSize = Game.getCurrentLevel().getMap().getCellSize();
+        double maxDistanceInPixels = 1.5 * cellSize;
+
+        List<Ennemy> ennemisProches = Game.getActiveEnemies().stream()
+                .filter(e -> !e.equals(this)) // Exclure le Buffer lui-même
+                .filter(e -> calculatePixelDistance(e) <= maxDistanceInPixels)
+                .collect(Collectors.toList());
+
+        for (Ennemy ennemi : ennemisProches) {
+            ennemi.setATKSpeed(ennemi.getATKSpeed() * 1.5); // Augmente la vitesse d'attaque
+            ennemi.setMovingSpeed(ennemi.getMovingSpeed() * 1.5);       // Augmente la vitesse de déplacement
+        }
+
+        // Appelle l'attaque normale sur la cible
+        super.attaquer(cible);
     }
 }
